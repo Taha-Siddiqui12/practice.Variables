b6542f4434ab89f31745cd0c05690421fa20f2a3
Taha-Siddiqui12 <77443779+Taha-Siddiqui12@users.noreply.github.com>
2021-01-15 20:42:23 +0900

    initial commit
    変数を(str, int, float, bolean)学習

---
+++ Variables.py
@@ -0,0 +1,25 @@
+
+#frst_name = "Taha"
+#last_name = "Siddiqui"
+#full_name = frst_name +" "+ last_name
+#print("Hello "+full_name)
+#print(type(name))
+
+#2.move on  int =interger(whole number)
+
+#age = 23
+#age = age + 1
+#age += 1
+#print("Your age is: "+str(age))
+#print(type(age))
+
+#3.move on float =floating point number(decimal number)
+
+#height = 172.5
+#print("Your height is: "+str(height)+"cm")
+#print(type(height))
+
+#4.move on boolean(true or false)
+human = True
+print("Are you a human: "+str(human))
+print(type(human))
