2c0004ac7f4bb07a2cebced1c29753730968fd22
Taha-Siddiqui12 <77443779+Taha-Siddiqui12@users.noreply.github.com>
2021-01-15 21:44:56 +0900

    14th
    リストのデータ型を学習

---
+++ lists.py
@@ -0,0 +1,20 @@
+
+food = ["pizza","hamburger","hotdog","spaghetti","ice"]
+
+food[0] ="sushi"
+
+#food.append("ice cream")
+#food.remove("hotdog")
+#food.pop()
+#food.insert(0,"cake")
+#food.sort()
+#food.clear()
+
+for x in food :
+    print(x)
+
+#print(food[0])
+#print(food[1])
+#print(food[2])
+#print(food[3])
+#print(food[4])
