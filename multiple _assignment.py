d3241594992cfcd1d28abf119728260f31721b78
Taha-Siddiqui12 <77443779+Taha-Siddiqui12@users.noreply.github.com>
2021-01-15 20:57:02 +0900

    Second Commit
    複数の値をまとめて代入(多重代入)

---
+++ multiple _assignment.py
@@ -0,0 +1,24 @@
+#creat by Taha
+#2021/1/1
+
+#name = "Taha"
+#age = 24
+#attractive = True
+
+#name,age,attractive = "Taha",24,True
+
+#print(name)
+#print(age)
+#print(attractive)
+
+#Spongbob = 38
+#Patrick = 30
+#Sandy = 30
+#Squidward = 36
+
+Spongebob = Patrick = Sandy = Squidward = 30
+
+print(Spongebob)
+print(Patrick)
+print(Sandy)
+print(Squidward)
