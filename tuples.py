9a3ed49050355259b4f3dd630c6e3224bc5b071a
Taha-Siddiqui12 <77443779+Taha-Siddiqui12@users.noreply.github.com>
2021-01-15 21:58:10 +0900

    16th commit
    タプルの構文を学習

---
+++ tuples.py
@@ -0,0 +1,14 @@
+
+
+
+student = ("Taha",24,"mele")
+
+print(student.count("Taha"))
+print(student.index("male"))
+
+for x in student:
+    print(x)
+
+if "Taha" in student:
+    print("Taha is here!")
+    
