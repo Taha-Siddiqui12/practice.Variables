98e0aa213dd38e2653e9a606e965a980594b3608
Taha-Siddiqui12 <77443779+Taha-Siddiqui12@users.noreply.github.com>
2021-01-15 22:00:09 +0900

    17th Commit
    set(集合型)を学習

---
+++ sets.py
@@ -0,0 +1,17 @@
+
+
+utensils = ["fork","spoon","knife"]
+dishes = {"bowl","plate","cop"}
+
+#utensils.add("napkin")
+#utensils.remove("fork")
+#utensils.clear()
+#dishes.update(dishes)
+#dinner_table = utensils.union(dishes)
+
+#print(utensils.difference(utensils))
+#print(utensils.intersetion))
+
+#for x in utensils:
+    #print(x)
+
