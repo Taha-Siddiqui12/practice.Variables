14c3214fd45f2a5727131ac74d81b2dc41cf2412
Taha-Siddiqui12 <77443779+Taha-Siddiqui12@users.noreply.github.com>
2021-01-15 21:16:35 +0900

    7th Commit
    文字列のスライス機能を学習

---
+++ string_siling.py
@@ -0,0 +1,19 @@
+name = "Taha Siddiqui"
+
+#first_name = name[0]
+#first_name = name[0:4]
+
+#first_name = name[:4]
+# last_name = name[4:]
+#funky_name =name[0:8:2]
+#reversed_name = name[::-1]
+
+#print(reversed_name)
+
+website1 = "http;//google.com"
+website2 = "http;//wikipedia.com"
+
+slice = slice(7,-4)
+
+print(website1[slice])
+print(website2[slice])
