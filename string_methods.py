92d88c1d222940ed90569729bd15d610083485b2
Taha-Siddiqui12 <77443779+Taha-Siddiqui12@users.noreply.github.com>
2021-01-15 21:08:21 +0900

    5th Commit
    Stringクラスの学習

---
+++ string_methods.py
@@ -0,0 +1,12 @@
+name = "Taha Siddiqui"
+
+#print(len(name))
+#print(name.find("T"))
+#print(name.capitalize())
+#print(name.upper())
+#print(name.lower())
+#print(name.isdigit())
+#print(name.isalpha())
+#print(name.count("i"))
+#print(name.replace("i","a"))
+#print(name*3)
