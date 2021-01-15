fa2527f83969aa79bf0ad40840419f41d9e91c84
Taha-Siddiqui12 <77443779+Taha-Siddiqui12@users.noreply.github.com>
2021-01-15 21:34:18 +0900

    11th Commit
    For文の学習

---
+++ for_loop.py
@@ -0,0 +1,19 @@
+import time
+
+#for i in range (10):
+    #print(i+1)
+
+#for i in range(50,100+1):
+        #print(i)
+
+#for i in range(50,100+1,2):
+        #print(i)
+
+#for i in "Taha Siddiqui":
+       #print(i)
+
+
+for seconds in range(10,0,-1):
+    print(seconds)
+    time.sleep(1)
+print("Happy Nwe Year")
